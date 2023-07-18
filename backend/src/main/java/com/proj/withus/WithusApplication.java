8d851af149011a0434fa254076e49b3b915d3e75
woochanhee <8dener@gmail.com>
2023-07-19 08:23:43 +0900

    [BE] chore: 프로젝트 초기 세팅

---
+++ backend/src/main/java/com/proj/withus/WithusApplication.java
@@ -0,0 +1,13 @@
+package com.proj.withus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithusApplication.class, args);
+	}
+
+}
