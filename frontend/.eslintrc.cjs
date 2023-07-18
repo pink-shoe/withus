e1bbff5c9d39268d903e7a7a6c55d269da03cec6
yebin0825 <yebin0825@naver.com>
2023-07-19 08:46:11 +0900

    [FE] chore: frontend 프로젝트 초기 세팅

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,30 @@
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+  env: { browser: true, commonjs:true, es6: true, node:true },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+  },
+}
