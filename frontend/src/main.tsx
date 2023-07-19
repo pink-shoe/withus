08ddb848f725379c851970657fdeacc7bad56109
yebin0825 <yebin0825@naver.com>
2023-07-19 14:02:11 +0900

    [FE] chore: 파일시스템대로 라우팅하도록 처리
    generouted 라이브러리를 활용하여 파일시스템대로 라우팅하도록 처리하고 예제 넣어둠

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './App.tsx';
+import './index.css';
+import { Routes } from '@generouted/react-router';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// import { createRoot } from 'react-dom/client';
+// import { Routes } from '@generouted/react-router';
+
+// const app = document.getElementById('app')!;
+// createRoot(app).render(<Routes />);
+
+const app = document.getElementById('root')!;
+ReactDOM.createRoot(app).render(<Routes />);
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import generouted from '@generouted/react-router/plugin';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig({ plugins: [react(), generouted()] });
