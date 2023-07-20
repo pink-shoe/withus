34ad7885fecfd7c3b8743ccc130659bbf0d07496
yebin0825 <yebin0825@naver.com>
2023-07-20 10:55:53 +0900

    [FE] chore: resolve 추가

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import generouted from '@generouted/react-router/plugin';
-
+import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({ plugins: [react(), generouted()] });
+
+resolve: {
+  alias: [{ find: '@', replacement: resolve(__dirname, './src') }];
+}
