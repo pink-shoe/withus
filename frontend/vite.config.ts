68a349794acfc99511b3cd3b48bf9eb5c3314a70
yebin0825 <yebin0825@naver.com>
2023-07-20 13:52:33 +0900

    [FE] chore: 디렉토리 접근 path resolve 추가

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import generouted from '@generouted/react-router/plugin';
-import { resolve } from 'path';
 // https://vitejs.dev/config/
-export default defineConfig({ plugins: [react(), generouted()] });
-
-resolve: {
-  alias: [{ find: '@', replacement: resolve(__dirname, './src') }];
-}
+export default defineConfig({
+  plugins: [
+    react(),
+    generouted({
+      source: {
+        routes: './src/pages/**/[\\w[-]*.{jsx,tsx}',
+        modals: './src/pages/**/[+]*.{jsx,tsx}',
+      },
+      output: './src/router.ts',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@components': '/src/components',
+      hooks: '/src/hooks',
+      lib: '/src/lib',
+      '@pages': '/src/pages',
+      stores: '/src/stores',
+      styles: '/src/styles',
+      types: '/src/types',
+      utils: '/src/utils',
+    },
+  },
+});
