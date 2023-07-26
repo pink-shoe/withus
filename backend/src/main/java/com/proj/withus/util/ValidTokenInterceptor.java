da1fe09731f7d29cdae2c3261d475d8cb2fe426d
dener8 <8dener@gmail.com>
2023-07-26 13:51:39 +0900

    [BE] feat: 소셜(카카오, 구글) 로그인 통합 이전, 임시적으로 인터셉터 관련 주석 처리
    로그인 테스트 오류를 임시적으로 방지하기 위해 주석처리
    후에 수정 예정

--- backend/src/main/java/com/proj/withus/util/ValidTokenInterceptor.java
+++ backend/src/main/java/com/proj/withus/util/ValidTokenInterceptor.java
@@ -1,41 +1,41 @@
-package com.proj.withus.util;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.servlet.HandlerInterceptor;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-@Slf4j
-public class ValidTokenInterceptor implements HandlerInterceptor {
-
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object Handler) throws Exception {
-        try {
-            String token = request.getHeader("Authorization").substring(7);
-            String reqURL = "https://kapi.kakao.com/v2/user/me";
-            URL url = new URL(reqURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-            conn.setRequestMethod("GET");
-            conn.setDoOutput(true);
-            conn.setRequestProperty("Authorization", "Bearer " + token); //전송할 header 작성, access_token 전송
-
-            //결과 코드가 200이라면 성공
-            int responseCode = conn.getResponseCode();
-
-            if (responseCode != 200) {
-                System.out.println("responseCode is not 200");
-                return false;
-            }
-        } catch (Exception e) {
-            log.info("interceptor exception");
-            return false;
-        }
-
-        System.out.println("responseCode is 200 !!");
-        return true;
-    }
-} // 만약에 google 등 다른 소셜 로그인 같이 넣을거면 메서드 분리 필요함 (지금은 그냥 카카오 api로 때려박음)
+//package com.proj.withus.util;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.net.HttpURLConnection;
+//import java.net.URL;
+//
+//@Slf4j
+//public class ValidTokenInterceptor implements HandlerInterceptor {
+//
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object Handler) throws Exception {
+//        try {
+//            String token = request.getHeader("Authorization").substring(7);
+//            String reqURL = "https://kapi.kakao.com/v2/user/me";
+//            URL url = new URL(reqURL);
+//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//
+//            conn.setRequestMethod("GET");
+//            conn.setDoOutput(true);
+//            conn.setRequestProperty("Authorization", "Bearer " + token); //전송할 header 작성, access_token 전송
+//
+//            //결과 코드가 200이라면 성공
+//            int responseCode = conn.getResponseCode();
+//
+//            if (responseCode != 200) {
+//                System.out.println("responseCode is not 200");
+//                return false;
+//            }
+//        } catch (Exception e) {
+//            log.info("interceptor exception");
+//            return false;
+//        }
+//
+//        System.out.println("responseCode is 200 !!");
+//        return true;
+//    }
+//} // 만약에 google 등 다른 소셜 로그인 같이 넣을거면 메서드 분리 필요함 (지금은 그냥 카카오 api로 때려박음)
