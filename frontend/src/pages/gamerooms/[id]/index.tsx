46fd4b5d9c0a507e9a4169b9e5499737cc5f5e7e
yebin0825 <yebin0825@naver.com>
2023-07-27 16:24:12 +0900

    [FE] feat: 채팅 컴포넌트 페이지에 적용

--- frontend/src/pages/gamerooms/[id]/index.tsx
+++ frontend/src/pages/gamerooms/[id]/index.tsx
@@ -1,55 +1,12 @@
-import React, { FC, useEffect, useMemo, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import html2canvas from 'html2canvas';
 import saveAs from 'file-saver';
-// import { ChatRoom } from "../components/chat/ChatRoom";
-// import { ControllBar } from '@components/session/ControllBar';
-// import { useDispatch, useSelector } from "react-redux";
-// import {
-//   setBalanceA,
-//   setBalanceB,
-//   setBalanceId,
-//   setUserInfoList,
-// } from "../stores/slices/meetingSlice";
-// import { MeetingRoomInfoRes } from "../apis/response/sessionRes";
 import { VideoStream } from '@components/VideoStream';
-// import { useWebSocket } from "../hooks/useWebSocket";
 import { useLocation } from 'react-router-dom';
-import { IUser, useOpenvidu } from 'hooks/useOpenvidu';
+import { useOpenvidu } from 'hooks/useOpenvidu';
 import { ControllBarContainer } from '@components/controllBar/ControllBarContainer';
-// import { AvatimeApi } from "../apis/avatimeApi";
-// import { VolumeController } from "../components/VolumeController";
-// import { useBGM } from "../hooks/useBGM";
-// import { AlertSnackbar } from "../components/AlertSnackbar";
-// import { FinalPickModal } from "../components/session/modal/FinalPickModal";
-// import { BalanceGameModal } from "../components/session/modal/BalanceGameModal";
-// import { PickStuffModal } from "../components/session/modal/PickStuffModal";
-
-interface IProps {}
-
-interface IUserInfo {
-  userId: number;
-  userName: string;
-  token: string;
-}
-
-interface IGameRoomInfo {
-  gameUserInfoList: IUserInfo[];
-}
-export let localUser: IUser;
-// export const USER_INFO_DATA: IGameRoomInfo = {
-//   gameUserInfoList: [
-//     {
-//       userId: 123,
-//       userName: 'test',
-//       token: '',
-//     },
-//     {
-//       userId: 456,
-//       userName: 'test2',
-//       token: '',
-//     },
-//   ],
-// };
+import ParticipantsContainer from '@components/participantsList/ParticipantListContainer';
+import ChatContainer from '@components/Chat/ChatContainer';
 export default function GameRoom() {
   const location = useLocation();
 
@@ -58,177 +15,32 @@ export default function GameRoom() {
     location.pathname.length
   );
 
-  const headCount = useState();
   const [roomId, setRoomId] = useState<string>(currentPath);
   const [userId, setUserId] = useState<number>(Math.floor(Math.random() * 100));
-  const [isMaster, setIsMaster] = useState<boolean>(true);
-  // const navigate = useNavigate();
-
-  // const [gameRoomInfo, setgameRoomInfo] = useState<IGameRoomInfo>({
-  //   gameUserInfoList: {
-  //     userId: 0,
-  //     nickname: 'test',
-  //   },
-  // });
-  // const gameRoomInfo = USER_INFO_DATA;
-  // useEffect(() => {
-  //   setgameRoomInfo(USER_INFO_DATA);
-  //   console.log(gameRoomInfo);
-  // }, []);
-  // useEffect(() => {
-  //   if (meetingRoomInfo) {
-  //     return;
-  //   }
-
-  //   // AvatimeApi.getInstance().getMeetingRoomInfo(
-  //   //   { meetingroom_id: roomId },
-  //   //   {
-  //   //     onSuccess: (data) => {
-  //   //       setMeetingRoomInfo(data);
-  //   //       dispatch(setUserInfoList(data.meeting_user_info_list));
-  //   //     },
-  //   //     navigate,
-  //   //   }
-  //   // );
-  // }, [dispatch, meetingRoomInfo, navigate, roomId]);
-
-  // const [opened, setOpened] = useState<boolean[]>([true, true]);
-  // const cntOpened = opened.filter((it) => it).length;
-
-  // const [lastPickModalOpen, setLastPickModalOpen] = useState(false);
-  // const [balanceGameModalOpen, setBalanceGameModalOpen] = useState(false);
-  // const [balanceResult, setBalanceResult] = useState<any[]>([]);
-  // const [pickStuffModalOpen, setPickStuffModalOpen] = useState(false);
-  // const [showSnack, setShowSnack] = useState(false);
-  // const [snackMessage, setSnackMessage] = useState('');
-  const [chatStatus, setChatStatus] = useState<boolean>();
+  const [isHost, setIsHost] = useState<boolean>(false);
+  const [userName, setUserName] = useState('name' + userId);
+  const [chatStatus, setChatStatus] = useState<boolean>(true);
   const [readyStatus, setReadyStatus] = useState<boolean>(false);
+  const [isEditUserName, setIsEditUserName] = useState<boolean>(false);
 
-  // useWebSocket({
-  //   onConnect(frame, client) {
-  //     let flag = true;
-  //     client.subscribe(`/topic/meeting/status/${roomId}`, function (response) {
-  //       console.log(response.body);
-  //       if (JSON.parse(response.body).last_pick_status) {
-  //         setSnackMessage('3초 후 최종 선택이 시작돼요!!');
-  //         setShowSnack(true);
-  //         setTimeout(() => {
-  //           setLastPickModalOpen(true);
-  //         }, 3000);
-  //       }
-  //     });
-
-  //     client.subscribe(`/topic/meeting/balance/${roomId}`, function (response) {
-  //       console.log('asdasd', response.body);
-  //       const res = JSON.parse(response.body);
-  //       if (res.id) {
-  //         setSnackMessage('3초 후 밸런스 게임이 시작돼요!!');
-  //         setShowSnack(true);
-  //         dispatch(setBalanceId(res.id));
-  //         dispatch(setBalanceA(res.a));
-  //         dispatch(setBalanceB(res.b));
-  //         setTimeout(() => {
-  //           setBalanceGameModalOpen(true);
-  //         }, 3000);
-  //       }
-  //     });
-
-  //     client.subscribe(`/topic/meeting/stuff/${roomId}`, function (response) {
-  //       console.log(response.body);
-  //       if (flag) {
-  //         flag = false;
-  //         setSnackMessage('3초 후 물건 고르기 게임이 시작돼요!!');
-  //         setShowSnack(true);
-  //         setTimeout(() => {
-  //           setPickStuffModalOpen(true);
-  //         }, 3000);
-  //       }
-  //     });
-
-  //     client.subscribe(`/topic/meeting/balance/result/${roomId}`, function (response) {
-  //       console.log(response.body);
-  //       const res = JSON.parse(response.body).balance_result;
-  //       setBalanceResult(res);
-  //     });
-  //   },
-  //   beforeDisconnected(frame, client) {
-  //     client.publish({
-  //       destination: '/app/meeting/leave',
-  //       body: JSON.stringify({
-  //         meetingroom_id: roomId,
-  //         user_id: userId,
-  //       }),
-  //     });
-  //   },
-  // });
-
-  const { publisher, streamList, onChangeCameraStatus, onChangeMicStatus } = useOpenvidu(
-    userId!,
-    roomId
-  );
-  const [messageList, setMessageList] = useState<any[]>([]);
-  const [message, setMessage] = useState('');
-
-  const onChangeMessage = (e: any) => {
-    setMessage(e.target.value);
-    console.log(e.target.value);
-  };
+  const { session, publisher, streamList, onChangeCameraStatus, onChangeMicStatus, sendMessage } =
+    useOpenvidu(userId!, roomId);
 
   const onChangeChatStatus = (chatStatus: boolean) => {
     setChatStatus(!chatStatus);
   };
+
   const onChangeReadyStatus = (readyStatus: boolean) => {
     setReadyStatus(!readyStatus);
   };
-  const sendMessage = () => {
-    if (message) {
-      let msg = message.replace(/ +(?= )/g, '');
-      if (msg !== '' && msg !== ' ') {
-        {
-          streamList?.map((stream: any, idx: number) => {
-            stream.userId === userId &&
-              stream.streamManager.stream.session.signal({
-                data: JSON.stringify({
-                  message: message,
-                  nickname: 'name' + userId,
-                  streamId: stream.streamManager.stream.streamId,
-                }),
-              });
-          });
-        }
-      }
-    }
-    setMessage('');
+
+  const onChangeUserName = (userName: string) => {
+    setUserName(userName);
   };
 
-  const receiveMessage = () => {
-    // streamList &&
-    // console.log('test', streamList);
-    streamList?.map((stream: any, idx: number) => {
-      // console.log('test', stream.streamManager);
-      let msgList = messageList;
-      stream.userId === userId &&
-        stream.streamManager &&
-        stream.streamManager.stream.session.on('signal:chat', (e: any) => {
-          console.log('test', e);
-          const data = JSON.parse(e.data);
-          msgList.push({
-            connectionId: e.from.connectionId,
-            nickname: data.nickname,
-            message: data.message,
-          });
-          console.log('test', msgList);
-        });
-      setMessageList(msgList);
-    });
+  const onChangeIsEditUserName = (isEditUserName: boolean) => {
+    setIsEditUserName(!isEditUserName);
   };
-  useEffect(() => {
-    receiveMessage();
-  }, [streamList]);
-  useEffect(() => {
-    console.log('test', messageList);
-  }, [messageList]);
-
   const divRef = useRef<HTMLDivElement>(null);
 
   const handleDownload = async () => {
@@ -247,23 +59,18 @@ export default function GameRoom() {
     }
   };
   return (
-    <section className={`w-full flex  justify-between min-h-screen max-h-fit`}>
+    <section className={`w-full flex justify-between h-full min-h-screen`}>
       {/* 참가자 목록 */}
-      <div id='participantsList' className='w-fit max-w-[1/6] bg-white'>
-        <div className='bg-[#112364] p-3 text-white whitespace-nowrap font-bold text-xl'>
-          현재 플레이어({streamList.length})
-        </div>
-        <div className='bg-white'>
-          {streamList.map((stream, idx) => {
-            console.log(streamList);
-            return (
-              <div key={idx} className=' border-bottom border-b-2 p-3'>
-                {stream.userName}
-              </div>
-            );
-          })}
-        </div>
-      </div>
+      <ParticipantsContainer
+        userId={userId}
+        userName={userName}
+        onChangeUserName={onChangeUserName}
+        publisher={publisher}
+        streamList={streamList}
+        readyStatus={readyStatus}
+        onChangeIsEditUserName={onChangeIsEditUserName}
+        type={'GAME'}
+      />
       {/* openvidu 화면 */}
       <div className=' w-1/2 h-full flex flex-col justify-between items-center pb-5'>
         <header className=''>
@@ -275,7 +82,7 @@ export default function GameRoom() {
               {streamList?.map((stream: any, idx: number) => {
                 // const userInfo = streamList.find((it: any) => it.userId === stream.userId);
                 return (
-                  <div className='w-full h-full'>
+                  <div className='w-full h-full' key={idx}>
                     <VideoStream
                       streamManager={stream.streamManager}
                       name={stream.userName}
@@ -287,94 +94,23 @@ export default function GameRoom() {
             </div>
           )}
         </div>
-
-        {/* <div>
-          <div>
-            <VolumeController />
-            <div />
-            {meetingRoomInfo && (
-              <ChatRoom
-                chatType='all'
-                isOpened={opened[0]}
-                onClickHeader={() => {
-                  setOpened((prev) => [!prev[0], prev[1]]);
-                }}
-                maxHeight={opened[0] && cntOpened === 1 ? '100%' : '50%'}
-                chattingRoomId={meetingRoomInfo.chattingroom_id}
-              />
-            )}
-            {meetingRoomInfo && (
-              <ChatRoom
-                chatType='gender'
-                isOpened={opened[1]}
-                onClickHeader={() => {
-                  setOpened((prev) => [prev[0], !prev[1]]);
-                }}
-                maxHeight={opened[1] && cntOpened === 1 ? '100%' : '50%'}
-                chattingRoomId={
-                  gender === 'M'
-                    ? meetingRoomInfo.men_chattingroom_id
-                    : meetingRoomInfo.women_chattingroom_id
-                }
-              />
-            )}
-          </div>
-        </div> */}
         <div className=' p-3'>
           <ControllBarContainer
-            isHost={isMaster}
+            isHost={isHost}
             onChangeMicStatus={onChangeMicStatus}
             onChangeCameraStatus={onChangeCameraStatus}
             onChangeChatStatus={onChangeChatStatus}
             onChangeReadyStatus={onChangeReadyStatus}
           />
         </div>
-        <button onClick={handleDownload}>다운로드</button>
+        {/* <button onClick={handleDownload}>다운로드</button> */}
       </div>
-      {/* <AlertSnackbar
-        open={showSnack}
-        onClose={() => setShowSnack(false)}
-        message={snackMessage}
-        alertColor='info'
-        type='alert'
+      <ChatContainer
+        chatStatus={chatStatus}
+        session={session}
+        publisher={publisher}
+        sendMessage={sendMessage}
       />
-      {lastPickModalOpen && <FinalPickModal isOpened={lastPickModalOpen} />}
-      {balanceGameModalOpen && (
-        <BalanceGameModal
-          isOpened={balanceGameModalOpen}
-          onClose={() => setBalanceGameModalOpen(false)}
-        />
-      )}
-      {pickStuffModalOpen && <PickStuffModal isOpened={pickStuffModalOpen} />} */}
-      {chatStatus ? (
-        <div id='participantsList' className=' w-80 h-full flex flex-col bg-white justify-between'>
-          <div className=' text-center bg-[#112364] p-3 text-white whitespace-nowrap font-bold text-xl'>
-            채팅창
-          </div>
-          <div className='w-full h-[90%] p-3'>
-            {messageList.map((message, i) => {
-              console.log(message);
-              return <div>{message}</div>;
-            })}
-          </div>
-          <div className='  bg-white'>
-            <div className='  flex w-full justify-center items-center'>
-              <input
-                className='w-full p-2 border-2 border-blue-800 rounded-md text-center focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 placeholder:text-slate-400'
-                placeholder='메세지를 입력해주세요.'
-                type='text'
-                value={message}
-                onChange={onChangeMessage}
-              />
-              <button className='whitespace-nowrap' onClick={sendMessage}>
-                전송
-              </button>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div></div>
-      )}
     </section>
   );
 }
--- frontend/src/pages/waitingrooms/[id]/index.tsx
+++ frontend/src/pages/waitingrooms/[id]/index.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import { VideoStream } from '@components/VideoStream';
 import { useLocation } from 'react-router-dom';
-import { IUser, useOpenvidu } from 'hooks/useOpenvidu';
+import { useOpenvidu } from 'hooks/useOpenvidu';
 import { ControllBarContainer } from '@components/controllBar/ControllBarContainer';
 import ParticipantsContainer from '@components/participantsList/ParticipantListContainer';
+import ChatContainer from '@components/Chat/ChatContainer';
 export default function WaitingRoom() {
   const location = useLocation();
 
@@ -14,139 +15,33 @@ export default function WaitingRoom() {
 
   const [roomId, setRoomId] = useState<string>(currentPath);
   const [userId, setUserId] = useState<number>(Math.floor(Math.random() * 100));
-  const [isMaster, setIsMaster] = useState<boolean>(false);
+  const [isHost, setIsHost] = useState<boolean>(false);
   const [userName, setUserName] = useState('name' + userId);
   const [chatStatus, setChatStatus] = useState<boolean>(true);
   const [readyStatus, setReadyStatus] = useState<boolean>(false);
   const [isEditUserName, setIsEditUserName] = useState<boolean>(false);
 
-  // useWebSocket({
-  //   onConnect(frame, client) {
-  //     let flag = true;
-  //     client.subscribe(`/topic/meeting/status/${roomId}`, function (response) {
-  //       console.log(response.body);
-  //       if (JSON.parse(response.body).last_pick_status) {
-  //         setSnackMessage('3초 후 최종 선택이 시작돼요!!');
-  //         setShowSnack(true);
-  //         setTimeout(() => {
-  //           setLastPickModalOpen(true);
-  //         }, 3000);
-  //       }
-  //     });
-
-  //     client.subscribe(`/topic/meeting/balance/${roomId}`, function (response) {
-  //       console.log('asdasd', response.body);
-  //       const res = JSON.parse(response.body);
-  //       if (res.id) {
-  //         setSnackMessage('3초 후 밸런스 게임이 시작돼요!!');
-  //         setShowSnack(true);
-  //         dispatch(setBalanceId(res.id));
-  //         dispatch(setBalanceA(res.a));
-  //         dispatch(setBalanceB(res.b));
-  //         setTimeout(() => {
-  //           setBalanceGameModalOpen(true);
-  //         }, 3000);
-  //       }
-  //     });
-
-  //     client.subscribe(`/topic/meeting/stuff/${roomId}`, function (response) {
-  //       console.log(response.body);
-  //       if (flag) {
-  //         flag = false;
-  //         setSnackMessage('3초 후 물건 고르기 게임이 시작돼요!!');
-  //         setShowSnack(true);
-  //         setTimeout(() => {
-  //           setPickStuffModalOpen(true);
-  //         }, 3000);
-  //       }
-  //     });
-
-  //     client.subscribe(`/topic/meeting/balance/result/${roomId}`, function (response) {
-  //       console.log(response.body);
-  //       const res = JSON.parse(response.body).balance_result;
-  //       setBalanceResult(res);
-  //     });
-  //   },
-  //   beforeDisconnected(frame, client) {
-  //     client.publish({
-  //       destination: '/app/meeting/leave',
-  //       body: JSON.stringify({
-  //         meetingroom_id: roomId,
-  //         user_id: userId,
-  //       }),
-  //     });
-  //   },
-  // });
-
-  const { publisher, streamList, onChangeCameraStatus, onChangeMicStatus } = useOpenvidu(
-    userId!,
-    roomId
-  );
-  const [messageList, setMessageList] = useState<any[]>([]);
-  const [message, setMessage] = useState('');
-
-  const onChangeMessage = (e: any) => {
-    setMessage(e.target.value);
-    console.log(e.target.value);
-  };
+  const { session, publisher, streamList, onChangeCameraStatus, onChangeMicStatus, sendMessage } =
+    useOpenvidu(userId!, roomId);
 
   const onChangeChatStatus = (chatStatus: boolean) => {
     setChatStatus(!chatStatus);
   };
+
   const onChangeReadyStatus = (readyStatus: boolean) => {
     setReadyStatus(!readyStatus);
   };
+
   const onChangeUserName = (userName: string) => {
     setUserName(userName);
   };
+
   const onChangeIsEditUserName = (isEditUserName: boolean) => {
     setIsEditUserName(!isEditUserName);
   };
-  const sendMessage = () => {
-    if (message) {
-      let msg = message.replace(/ +(?= )/g, '');
-      if (msg !== '' && msg !== ' ') {
-        {
-          publisher.signal({
-            data: JSON.stringify({
-              message: message,
-              nickname: userName,
-              streamId: publisher.stream.streamId,
-            }),
-          });
-          // streamList?.map((stream: any, idx: number) => {
-          //   stream.userId === userId &&
-          //     stream.streamManager.stream.session.signal({
-
-          //     });
-          // });
-        }
-      }
-    }
-    setMessage('');
-  };
-
-  const receiveMessage = () => {
-    let msgList = messageList;
-
-    publisher.stream.session.on('signal:chat', (e: any) => {
-      console.log('test', e);
-      const data = JSON.parse(e.data);
-      msgList.push({
-        connectionId: e.from.connectionId,
-        nickname: data.nickname,
-        message: data.message,
-      });
-      console.log('test', msgList);
-    });
-  };
-
-  useEffect(() => {
-    receiveMessage();
-  });
 
   return (
-    <section className={`w-full flex overflow-y-hidden  justify-between h-screen`}>
+    <section className={`w-full flex  justify-between h-screen`}>
       {/* 참가자 목록 */}
       <ParticipantsContainer
         userId={userId}
@@ -156,25 +51,23 @@ export default function WaitingRoom() {
         streamList={streamList}
         readyStatus={readyStatus}
         onChangeIsEditUserName={onChangeIsEditUserName}
+        type={'WAIT'}
       />
       {/* openvidu 화면 */}
       <div className=' h-full flex flex-col justify-between pb-5'>
         <header className=''>
           <div className=' text-white font-extrabold text-6xl text-center py-10'>[] with us</div>
         </header>
-        {publisher && (
-          <div className='w-full'>
-            <VideoStream
-              streamManager={publisher.streamManager}
-              name={publisher.userName}
-              me={publisher.userId === userId}
-            />
-          </div>
-        )}
-
+        <div className='aspect-[4/3]'>
+          {publisher && (
+            <div className='w-full'>
+              <VideoStream streamManager={publisher} name={userName} me={true} />
+            </div>
+          )}
+        </div>
         <div className=' p-3'>
           <ControllBarContainer
-            isHost={isMaster}
+            isHost={isHost}
             onChangeMicStatus={onChangeMicStatus}
             onChangeCameraStatus={onChangeCameraStatus}
             onChangeChatStatus={onChangeChatStatus}
@@ -182,31 +75,12 @@ export default function WaitingRoom() {
           />
         </div>
       </div>
-
-      {chatStatus ? (
-        <div id='participantsList' className=' w-80'>
-          <div className='text-center bg-[#112364] p-3 text-white whitespace-nowrap font-bold text-xl'>
-            채팅창
-          </div>
-          <div className='bg-white h-full'>
-            <div className='w-full h-5/6'></div>
-            <div className='flex w-full justify-center items-center'>
-              <input
-                className='w-full p-2 border-2 border-blue-800 rounded-md text-center focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 placeholder:text-slate-400'
-                placeholder='메세지를 입력해주세요.'
-                type='text'
-                value={message}
-                onChange={onChangeMessage}
-              />
-              <button className='whitespace-nowrap' onClick={sendMessage}>
-                전송
-              </button>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div></div>
-      )}
+      <ChatContainer
+        chatStatus={chatStatus}
+        session={session}
+        publisher={publisher}
+        sendMessage={sendMessage}
+      />
     </section>
   );
 }
