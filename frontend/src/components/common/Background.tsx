743e14be741892f6c9c8f3e467cbc310b685494e
337sally <337sally@naver.com>
2023-08-02 00:36:49 +0900

    [FE] feat: css 계속 수정 중

--- frontend/src/components/common/Background.tsx
+++ frontend/src/components/common/Background.tsx
@@ -7,12 +7,12 @@ export default function Background() {
     <Fragment>
       <Header />
       <div className='flex justify-center place-items-center h-screen bg-red-200'>
-        <div className='h-[41rem] w-[75rem]'>
+        <div className='h-[38rem] w-[75rem]'>
           <div>
             <div className='flex justify-center place-items-center drop-shadow-xl rounded-md bg-yellow-100 h-[2.5rem]'></div>
-            <Logo />
           </div>
-            <div className='flex justify-center place-items-center h-5/6 w-5/6 mx-10 bg-white'>
+          <Logo />
+            <div className='flex justify-center place-items-center h-[35rem] w-[70rem] ms-[2.5rem] bg-white'>
               dddddddd
             </div>
           <div className='flex justify-center place-items-center drop-shadow-lg rounded-md bg-yellow-100 h-[2.5rem]'></div>
