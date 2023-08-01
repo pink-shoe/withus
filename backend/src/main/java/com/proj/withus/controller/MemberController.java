8f3b9aa50684d66b1b33e731f0dd7cddab636f8b
jieun21124 <jieun21124@naver.com>
2023-08-01 19:02:22 +0900

    [BE] refactor: mypage 기능 리팩토링

--- backend/src/main/java/com/proj/withus/controller/MemberController.java
+++ backend/src/main/java/com/proj/withus/controller/MemberController.java
@@ -1,24 +1,15 @@
 package com.proj.withus.controller;
 
 import com.proj.withus.domain.Album;
-import com.proj.withus.service.AlbumServiceImpl;
-import com.proj.withus.util.JwtUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.proj.withus.domain.Member;
+import com.proj.withus.service.AlbumServiceImpl;
 import com.proj.withus.service.MemberServiceImpl;
-
+import com.proj.withus.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -26,13 +17,9 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/members")
 public class MemberController {
 
-    @Autowired
-    private MemberServiceImpl memberServiceImpl;
-
-    @Autowired
-    private AlbumServiceImpl albumServiceImpl;
-
-    private JwtUtil jwtUtil = new JwtUtil();
+    private final MemberServiceImpl memberServiceImpl;
+    private final AlbumServiceImpl albumServiceImpl;
+    private final JwtUtil jwtUtil;
 
     @GetMapping
     private ResponseEntity<?> getMemberInfo(@RequestHeader("Authorization") String jwtToken) {
@@ -52,6 +39,9 @@ public class MemberController {
 
         if (updatedInfo == null) {
             return new ResponseEntity<>("유저 정보 찾을 수 없음", HttpStatus.BAD_REQUEST);
+        }
+        if (!updatedInfo.getNickname().equals(nickname)) {
+            return new ResponseEntity<>("닉네임을 수정하지 못함", HttpStatus.BAD_REQUEST);
         }
         return ResponseEntity.ok().body(updatedInfo);
     }
