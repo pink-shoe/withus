c42841d8d4b43cbdbb47fe4da63a90d48b8464a1
jini11 <jieun21124@naver.com>
2023-08-03 08:44:28 +0900

    [BE] feat: 방 기능 swagger2 구현 example 속성 추가할 것

--- backend/src/main/java/com/proj/withus/domain/dto/CreateRoomReq.java
+++ backend/src/main/java/com/proj/withus/domain/dto/CreateRoomReq.java
@@ -1,15 +1,23 @@
 package com.proj.withus.domain.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
 @RequiredArgsConstructor
+@ApiModel(description = "방 생성 정보")
 public class CreateRoomReq {
 
+    @ApiModelProperty(value = "유저 id", required = true, example = "1")
     private final Long id; // memberId
+
+    @ApiModelProperty(value = "방 타입(팀전, 협동전)", required = true, example = "coop")
     private final int roomType;
+
+    @ApiModelProperty(value = "게임 총 라운드", required = true, example = "5")
     private final int roomRound = 5; // room round 5로 고정?
 
 }
