bda875a1391b8f94ac092f7cba44ae386687211c
dener8 <8dener@gmail.com>
2023-08-03 14:53:26 +0900

    [BE] refactor: 방 생성 api swagger 설정 변경

--- backend/src/main/java/com/proj/withus/domain/dto/CreateRoomReq.java
+++ backend/src/main/java/com/proj/withus/domain/dto/CreateRoomReq.java
@@ -14,10 +14,10 @@ public class CreateRoomReq {
     @ApiModelProperty(value = "유저 id", required = true, example = "1")
     private final Long id; // memberId
 
-    @ApiModelProperty(value = "방 타입(팀전, 협동전)", required = true, example = "coop")
+    @ApiModelProperty(value = "방 타입(팀전, 협동전)", required = true, example = "1")
     private final int roomType;
 
     @ApiModelProperty(value = "게임 총 라운드", required = true, example = "5")
-    private final int roomRound = 5; // room round 5로 고정?
+    private final int roomRound; // room round 5로 고정?
 
 }
