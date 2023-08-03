0a03cf3177e99ab13a1e811c17cf0bb9bff5a56a
337ccc <337ccc@gmail.com>
2023-08-03 13:55:39 +0900

    [FE] fix: 코드 수정 중

--- frontend/src/components/common/Background.tsx
+++ frontend/src/components/common/Background.tsx
@@ -4,17 +4,14 @@ import Board from './Board';
 
 interface IBackgroundProps {
   children: React.ReactNode;
-  boardType: any;
 }
 
-export default function Background({children, boardType}: IBackgroundProps) {
+export default function Background({children}: IBackgroundProps) {
   return (
     <Fragment>
       <UserHeader />
       <div className='flex justify-center place-items-center h-screen bg-[#F9C7C8]'>
-        <Board boardType={boardType}>
-          {children}
-        </Board>
+        {children}
       </div>
     </Fragment>
   )
--- frontend/src/pages/lobby.tsx
+++ frontend/src/pages/lobby.tsx
@@ -5,7 +5,8 @@ import { useNavigate } from 'react-router-dom';
 import { myPageApi } from 'apis/myPageApi';
 import { useAtom } from 'jotai';
 import { userAtom } from 'stores/index';
-import Background from '@components/common/Background';
+import Background from '../components/common/Background';
+import Board from '../components/common/Board';
 
 export default function Lobby() {
   const [user, setUser] = useAtom(userAtom);
@@ -64,55 +65,57 @@ export default function Lobby() {
 
   return (
     <Fragment>
-      <Background boardType={'LOBBY'} >
-      <div className='flex flex-auto justify-center content-center'>
-          <Fragment>
-            <button
-              onClick={openMakeModal}
-              className='bg-[#FF8D8D] hover:bg-red-500 me-10 aspect-square h-96 rounded-xl font-medium font-kdisplay text-4xl text-white'
-            >
-              방 만들기
-            </button>
-            <SettingModalContainer
-              isUpdateModal={false}
-              openModal={makeRoomModal}
-              closeModal={closeMakeModal}
-            ></SettingModalContainer>
-          </Fragment>
-          <Fragment>
-            <button
-              onClick={openEnterModal}
-              className='bg-[#8D98FF] hover:bg-violet-700 ms-10 aspect-square h-96 rounded-xl font-medium font-kdisplay text-4xl text-white'
-            >
-              방 참여하기
-            </button>
-            <Modal openModal={enterRoomModal} closeModal={closeEnterModal}>
-              <p className='text-[#514148] font-kdisplay font-medium text-4xl mb-10 text-center'>
-                참여 코드
-              </p>
-              <div className='flex mb-7'>
-                <span className='me-5 font-kdisplay font-medium text-2xl flex items-center'>
-                  참여코드
-                </span>
-                <input
-                  className='p-2 border-2 w-[19rem] border-[#8D98FF] focus:outline-none focus:border-violet-500 focus:ring-1 focus:ring-violet-500 rounded-md font-medium text-2xl text-center text-[#514148] font-kdisplay'
-                  placeholder='참여코드 입력'
-                  type='text'
-                  value={enterCode}
-                  onChange={writeCode}
-                />
-              </div>
-              <div className='flex justify-center'>
-                <button
-                  onClick={onClickParticipantBtn}
-                  className='bg-[#8D98FF] hover:bg-violet-700 w-72 h-12 rounded-md font-medium font-kdisplay text-2xl text-white'
-                >
-                  참여
-                </button>
-              </div>
-            </Modal>
-          </Fragment>
-        </div>
+      <Background>
+        <Board boardType={'LOBBY'}>
+          <div className='flex flex-auto justify-center content-center'>
+            <Fragment>
+              <button
+                onClick={openMakeModal}
+                className='bg-[#FF8D8D] hover:bg-red-500 me-10 aspect-square h-96 rounded-xl font-medium font-kdisplay text-4xl text-white'
+              >
+                방 만들기
+              </button>
+              <SettingModalContainer
+                isUpdateModal={false}
+                openModal={makeRoomModal}
+                closeModal={closeMakeModal}
+              ></SettingModalContainer>
+            </Fragment>
+            <Fragment>
+              <button
+                onClick={openEnterModal}
+                className='bg-[#8D98FF] hover:bg-violet-700 ms-10 aspect-square h-96 rounded-xl font-medium font-kdisplay text-4xl text-white'
+              >
+                방 참여하기
+              </button>
+              <Modal openModal={enterRoomModal} closeModal={closeEnterModal}>
+                <p className='text-[#514148] font-kdisplay font-medium text-4xl mb-10 text-center'>
+                  참여 코드
+                </p>
+                <div className='flex mb-7'>
+                  <span className='me-5 font-kdisplay font-medium text-2xl flex items-center'>
+                    참여코드
+                  </span>
+                  <input
+                    className='p-2 border-2 w-[19rem] border-[#8D98FF] focus:outline-none focus:border-violet-500 focus:ring-1 focus:ring-violet-500 rounded-md font-medium text-2xl text-center text-[#514148] font-kdisplay'
+                    placeholder='참여코드 입력'
+                    type='text'
+                    value={enterCode}
+                    onChange={writeCode}
+                  />
+                </div>
+                <div className='flex justify-center'>
+                  <button
+                    onClick={onClickParticipantBtn}
+                    className='bg-[#8D98FF] hover:bg-violet-700 w-72 h-12 rounded-md font-medium font-kdisplay text-2xl text-white'
+                  >
+                    참여
+                  </button>
+                </div>
+              </Modal>
+            </Fragment>
+          </div>
+        </Board>
       </Background>
     </Fragment>
   );
