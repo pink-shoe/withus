329ef51296fd463fa9870621cf8c2092c81ca3bb
337ccc <337ccc@gmail.com>
2023-08-03 10:17:00 +0900

    [FE] fix: 주석 추가

--- frontend/src/components/common/Background.tsx
+++ frontend/src/components/common/Background.tsx
@@ -12,6 +12,7 @@ export default function Background({children, isBoard}: IBackgroundProps) {
     <Fragment>
       <UserHeader />
       <div className='flex justify-center place-items-center h-screen bg-[#F9C7C8]'>
+        {/* Background를 사용할 페이지에서 isBoard를 game, wait, lobby 중 하나로 지정해서 사용 */}
         <Board isBoard={isBoard}>
           {children}
         </Board>
