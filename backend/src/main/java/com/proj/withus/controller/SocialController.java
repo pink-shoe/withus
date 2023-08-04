35fce69b469e67172fb26ad4ed774044cd1ade0c
dener8 <8dener@gmail.com>
2023-08-04 16:54:57 +0900

    [BE] refactor: pathvariable snake case로 통일

--- backend/src/main/java/com/proj/withus/controller/SocialController.java
+++ backend/src/main/java/com/proj/withus/controller/SocialController.java
@@ -37,7 +37,7 @@ public class SocialController {
 
     @ApiOperation(value = "소셜 로그인", notes = "인가 코드, 로그인 타입으로 소셜 로그인을 진행한다.")
     @ResponseBody
-    @GetMapping("/api/oauth/{login-type}") // pathvariable로 애초에 loginType 받고, 이걸로 jwt 만들기
+    @GetMapping("/api/oauth/{login_type}") // pathvariable로 애초에 loginType 받고, 이걸로 jwt 만들기
     public ResponseEntity<?> callback(
             @ApiParam(value = "소셜 서버에서 전달 받은 인가 코드 (Authorization code)", required = true) @RequestParam String code,
             @ApiParam(value = "소셜 이름 (kakao 혹은 google)", required = true) @PathVariable(name = "login-type") String loginType) {
