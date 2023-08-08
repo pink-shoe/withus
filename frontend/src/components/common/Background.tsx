0c3f2415445066a6d287b629d2d8266ad75cbf02
yebin0825 <yebin0825@naver.com>
2023-08-08 10:49:36 +0900

    [FE] fix: resolve error

--- frontend/src/components/common/Background.tsx
+++ frontend/src/components/common/Background.tsx
@@ -4,14 +4,14 @@ import UserHeader from './UserHeader';
 interface IBackgroundProps {
   // isLobbyDropdown true는 로비 항목이 없고, false는 로비 항목이 있음
   isLobbyDropdown?: any;
-  isLobbyPage: boolean;
+  isLobbyPage?: boolean;
   children: React.ReactNode;
 }
 
-export default function Background({ isLobbyDropdown, isLobbyPage, children }: IBackgroundProps) {
+export default function Background({ children }: IBackgroundProps) {
   return (
     <Fragment>
-      <UserHeader isLobbyDropdown={isLobbyDropdown} />
+      <UserHeader />
       <div className='flex justify-center place-items-center h-screen bg-[#F9C7C8]'>{children}</div>
     </Fragment>
   );
