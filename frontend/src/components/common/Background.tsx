ab02cbf4d6724e09b32a33f139bd5d62f7c3175f
minyoung <122516219+337ccc@users.noreply.github.com>
2023-08-09 06:34:58 +0900

    Feature/337ccc/front css (#54)
    * [FE] feat: Background 디자인 적용 & 로고 만들기
    
    * [FE] feat: css 계속 수정 중
    
    * [FE] feat: WIP
    
    * [FE] feat: 수정 중
    
    * [FE] fix: 디자인 수정 중
    
    * [FE] fix: 디자인 수정 중
    
    * [FE] fix: 프론트 디자인 적용 중
    
    * [FE] fix: 주석 추가
    
    * [FE] fix: 화면 타입 지정(GAME, WAIT, LOBBY)
    
    * [FE] fix: boardType으로 변경
    
    * [FE] fix: 코드 수정 중
    
    * [FE] fix: 디자인 수정 중
    
    * [FE] fix: 디자인 수정 및 컴포넌트 추가
    
    * [FE] fix: 디자인 적용 중
    
    * [FE] fix: 디자인 수정 중
    
    * [FE] fix: 아이콘, dropdown 수정 중
    
    * [FE] fix: 새로 올려준 nginx와 Dockerfile로 수정
    
    * [FE] fix: 아이콘 크기 및 색깔 완료, 레이아웃은 진행 중
    
    * [FE] fix: 수정 중
    
    * [FE] fix: 모달 제작 중
    
    * [FE] fix: 갑자기 게임이 종료된 상황에 나타나는 모달 제작 완료
    
    * [FE] fix: 대기실 레이아웃 수정 중
    
    * [FE] fix: 협동전 옆에 '판' 존재 다르게 설정 완료
    
    * [FE] fix: 화면 크기 변경
    
    ---------
    
    Co-authored-by: 337sally <337sally@naver.com>

--- frontend/src/components/common/Background.tsx
+++ frontend/src/components/common/Background.tsx
@@ -8,11 +8,31 @@ interface IBackgroundProps {
   children: React.ReactNode;
 }
 
-export default function Background({ children }: IBackgroundProps) {
+export default function Background({isLobbyDropdown, isLobbyPage, children}: IBackgroundProps) {
   return (
     <Fragment>
-      <UserHeader />
-      <div className='flex justify-center place-items-center h-screen bg-[#F9C7C8]'>{children}</div>
+      {isLobbyPage ? (
+        // 로비 페이지와 같이 드롭다운이 필요한 경우
+        <div className="min-w-[480px] white">
+          <Fragment>
+            <div className="hover:text-red-300">
+              <UserHeader isLobbyDropdown={isLobbyDropdown} />
+            </div>
+            <div className="flex justify-center place-items-center h-[730px] tall:h-screen bg-[#F9C7C8]">
+              {children}
+            </div>
+          </Fragment>
+        </div>
+      ) : (
+        // 드롭다운이 필요 없는 경우
+        <div className="min-w-[865px] white">
+          <Fragment>
+            <div className="flex justify-center place-items-center h-[730px] tall:h-screen bg-[#F9C7C8]">
+              {children}
+            </div>
+          </Fragment>
+        </div>
+      )}
     </Fragment>
   );
 }
--- frontend/src/components/common/UserHeader.tsx
+++ frontend/src/components/common/UserHeader.tsx
@@ -1,4 +1,6 @@
 // 대기실 입장 전까지 Header
+// User 아이콘이 있고, 이 아이콘을 클릭하면 Dropdown이 나옴
+
 import { Link, useNavigate } from 'react-router-dom';
 import UserDropdown from './UserDropdown';
 
@@ -23,6 +25,7 @@ export default function UserHeader({ isLobbyDropdown }: IUserHeaderProps) {
         <div className='cursor-pointer z-40'>
           {isLobbyDropdown ? (
             // 로비 페이지에서 드롭다운
+            // '로비'라는 항목이 없음
             <UserDropdown>
               <ul className='hover:text-red-300 font-kdisplay text-xl'>
                 <li className='bg-white text-center rounded-t-lg border-[#FA8D8D] mt-2 border-2 h-9 py-1 px-4 text-[#FA8D8D] hover:text-black'>
