6e98e7d030443bffade1eda5a54ab94a1de4e464
Noopy94 <ldh9411@hanmail.net>
2023-08-14 15:30:47 +0900

    [FE] feat: NickName Change Modal update

--- frontend/src/pages/mypage/index.tsx
+++ frontend/src/pages/mypage/index.tsx
@@ -7,14 +7,31 @@ import InputComponent from '@components/common/InputComponent';
 import { updateMemberApi } from 'apis/memberApi';
 import Board from '@components/common/Board';
 import Background from '@components/common/Background';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from '@components/common/Modal';
+import { deleteMemberApi } from 'apis/memberApi';
+import { getMemberApi } from 'apis/memberApi';
 
 export default function Mypage() {
   const [user, setUser] = useAtom(userAtom);
+  const [newNickname, setNewNickname] = useState('');
   const navigate = useNavigate();
   const [isNickNameModalOpen, setIsNickNameModalOpen] = useState(false);
   const [isWithdrawModalOpen, setIsWithdrawModalOpen] = useState(false);
+
+  // 비정상적인 접근 차단 & 새로고침마다 유저 정보 재확인
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+
+    if (!accessToken) {
+      navigate('/login');
+      // accessToken이 없으면 게스트 or 게스트조차 아닌 로그아웃 상태이니 login 이동
+    } else {
+      getMemberApi(setUser).catch((error) => {
+        console.log('Error fetching data:', error);
+      });
+    }
+  }, []);
 
   console.log(`닉네임 확인: ${user.nickname}`);
 
@@ -30,23 +47,38 @@ export default function Mypage() {
     setIsWithdrawModalOpen(true);
   };
 
-  const CloseWithdraweModal = () => {
+  const CloseWithdrawModal = () => {
     setIsWithdrawModalOpen(false);
   };
 
-  const onChangeNickName = (data: any) => {
-    setUser((prevUser) => ({
-      ...prevUser,
-      nickname: data.target.value,
-    }));
+  const onChangeNickName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewNickname(event.target.value);
   };
 
   const UpdateNickName = () => {
-    updateMemberApi(user.nickname)
-      .then(() => {})
+    updateMemberApi(newNickname)
+      .then(() => {
+        setUser((prevUser) => ({
+          ...prevUser,
+          nickname: newNickname,
+        }));
+        CloseNickNameModal();
+      })
       .catch((error) => {
         console.error('Error updating nickname:', error);
       });
+  };
+
+  const WithdrawAccount = async () => {
+    try {
+      await deleteMemberApi(user.nickname);
+      localStorage.removeItem('token');
+      navigate('/login');
+      console.log('성공');
+      CloseWithdrawModal();
+    } catch (error) {
+      console.log('실패:', error);
+    }
   };
 
   const onClickGoLobby = () => {
@@ -63,25 +95,62 @@ export default function Mypage() {
               <InputComponent
                 type='nickname'
                 label='변경할 닉네임'
-                value={user.nickname} // Access the nickname from the userAtom
                 placeholder={user.nickname}
                 onChange={onChangeNickName}
               />
             </div>
-            <ButtonComponent type='isBig' onClick={UpdateNickName}>
+            <ButtonComponent type='isBig' onClick={OpenNickNameModal}>
               닉네임 변경하기
             </ButtonComponent>
+            <Modal openModal={isNickNameModalOpen} isSettingModal={false}>
+              <p className='font-kdisplay text-xl flex justify-center'>
+                정말로 닉네임을 변경하시겠습니까?
+              </p>
+              <div className='flex justify-center mt-4'>
+                <button
+                  className='bg-[#8D98FF] font-kdisplay text-2xl w-24 h-12 text-white rounded-lg hover:bg-violet-700 mr-2'
+                  onClick={UpdateNickName}
+                >
+                  예
+                </button>
+                <button
+                  className='bg-[#FA8D8D] font-kdisplay text-2xl w-24 h-12 text-white rounded-lg hover:bg-red-500 ml-2'
+                  onClick={CloseNickNameModal}
+                >
+                  아니오
+                </button>
+              </div>
+            </Modal>
             <ButtonComponent type='isBig' onClick={onClickGoLobby}>
               로비로 돌아가기
             </ButtonComponent>
             <div className='flex justify-end'>
-              <Link
+              <div
                 className='font-kdisplay text-lg text-gray-500 pr-4 pt-8 hover:text-black'
-                to='/login'
+                onClick={OpenWithdrawModal}
               >
                 회원탈퇴
-              </Link>
+              </div>
             </div>
+            <Modal openModal={isWithdrawModalOpen} isSettingModal={false}>
+              <p className='font-kdisplay text-xl flex justify-center'>
+                정말로 회원탈퇴를 하시겠습니까?
+              </p>
+              <div className='flex justify-center mt-4'>
+                <button
+                  className='bg-[#8D98FF] font-kdisplay text-2xl w-24 h-12 text-white rounded-lg hover:bg-violet-700 mr-2'
+                  onClick={WithdrawAccount}
+                >
+                  예
+                </button>
+                <button
+                  className='bg-[#FA8D8D] font-kdisplay text-2xl w-24 h-12 text-white rounded-lg hover:bg-red-500 ml-2'
+                  onClick={CloseWithdrawModal}
+                >
+                  아니오
+                </button>
+              </div>
+            </Modal>
           </Container>
         </Board>
       </Background>
