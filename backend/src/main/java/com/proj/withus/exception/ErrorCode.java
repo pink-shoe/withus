14b9b3323fe6a47d467f5df7fa98f2a9311e9dc7
jini11 <jieun21124@naver.com>
2023-08-17 08:49:53 +0900

    [BE] fix: 마지막 라운드 status 변경

--- backend/src/main/java/com/proj/withus/exception/ErrorCode.java
+++ backend/src/main/java/com/proj/withus/exception/ErrorCode.java
@@ -34,7 +34,7 @@ public enum ErrorCode {
 
     VOTE_FAIL(HttpStatus.BAD_REQUEST, "투표가 반영되지 않았습니다."),
 
-    LAST_ROUND(HttpStatus.BAD_REQUEST, "마지막 라운드 입니다."),
+    LAST_ROUND(HttpStatus.NO_CONTENT, "마지막 라운드 입니다."),
 
     /*
     찬희
