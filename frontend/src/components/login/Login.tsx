aa85d809e71b7537421a36ab90de77017714484e
minyoung <122516219+337ccc@users.noreply.github.com>
2023-08-16 11:07:21 +0900

    Feature/337ccc/front css (#101)
    * [FE] feat: Background 디자인 적용 & 로고 만들기
    
    * [FE] feat: css 계속 수정 중
    
    * [FE] feat: WIP
    
    * [FE] feat: 수정 중
    
    * [FE] fix: 디자인 수정 중
    
    * [FE] fix: 디자인 수정 중
    
    * [FE] fix: 프론트 디자인 적용 중
    
    * [FE] fix: 주석 추가
    
    * [FE] fix: 화면 타입 지정(GAME, WAIT, LOBBY)
    
    * [FE] fix: boardType으로 변경
    
    * [FE] fix: 코드 수정 중
    
    * [FE] fix: 디자인 수정 중
    
    * [FE] fix: 디자인 수정 및 컴포넌트 추가
    
    * [FE] fix: 디자인 적용 중
    
    * [FE] fix: 디자인 수정 중
    
    * [FE] fix: 아이콘, dropdown 수정 중
    
    * [FE] fix: 새로 올려준 nginx와 Dockerfile로 수정
    
    * [FE] fix: 아이콘 크기 및 색깔 완료, 레이아웃은 진행 중
    
    * [FE] fix: 수정 중
    
    * [FE] fix: 모달 제작 중
    
    * [FE] fix: 갑자기 게임이 종료된 상황에 나타나는 모달 제작 완료
    
    * [FE] fix: 대기실 레이아웃 수정 중
    
    * [FE] fix: 협동전 옆에 '판' 존재 다르게 설정 완료
    
    * [FE] fix: 화면 크기 변경
    
    * [FE] fix: 글씨체 변경
    
    * [FE] fix: 게임 레이아웃 변경, 앨범 로고 추가
    
    * [FE] fix: 로비 페이지 오타 수정
    
    * [FE] fix: 레이아웃 수정 중
    
    * [FE] fix: 세부적인 디자인 수정, 모달 제작 중
    
    * [FE] fix: 로그인 페이지 css 수정
    
    * [FE] feat: 라운드 모달 제작 중
    
    * [FE] feat: MVP 모달 제작 중
    
    * [FE] fix: MVP 모달 수정 중
    
    * [FE] fix: 모달 수정
    
    * [FE] fix: MVP 모달 수정 중
    
    * [FE] fix: MVP 모달 함수 수정
    
    * [FE] fix: MVP 모달 수정 중
    
    * [FE] fix: MVP 완료
    
    * [FE] fix: MVP 수정
    
    * [FE] fix: MVP 수정
    
    * [FE] fix: conflict 해결 중
    
    * [FE] fix: EndGameModal 삭제
    
    ---------
    
    Co-authored-by: 337sally <337sally@naver.com>

--- frontend/src/components/login/Login.tsx
+++ frontend/src/components/login/Login.tsx
@@ -6,7 +6,8 @@ export default function Login() {
   const GOOGLE_GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLECLIENT_ID;
 
   return (
-    <div className='flex flex-col justify-center h-screen'>
+    // 화면 밖으로 해당 div가 나가는 것 h-full로 변경함
+    <div className='flex flex-col justify-center h-full'>
       <div className='flex flex-wrap justify-center mb-10'>
         <p className='font-kdisplay text-4xl flex hover:text-[#FF8D8D]'>나만의 방과 사진첩을</p>
         <p className='font-kdisplay text-4xl flex hover:text-[#FF8D8D]'>구경해보세요!!</p>
