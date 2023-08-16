8d3f5f38fafa1389a442d6f5810e043ae6567777
Noopy94 <ldh9411@hanmail.net>
2023-08-16 11:15:44 +0900

    [FE] fix: merging

--- frontend/src/components/common/Background.tsx
+++ frontend/src/components/common/Background.tsx
@@ -11,31 +11,35 @@ interface IBackgroundProps {
   children: React.ReactNode;
 }
 
-export default function Background({backgroundType, isLobbyDropdown, children}: IBackgroundProps) {
-return (
+export default function Background({
+  backgroundType,
+  isLobbyDropdown,
+  children,
+}: IBackgroundProps) {
+  return (
     <Fragment>
       {backgroundType === 'LOBBY' ? (
         // 로비 페이지와 같이 드롭다운이 필요한 경우
-        <div className="min-w-[480px] white">
+        <div className='min-w-[480px] white'>
           <Fragment>
-            <div className="hover:text-red-300">
+            <div className='hover:text-red-300'>
               <UserHeader isLobbyDropdown={isLobbyDropdown} />
             </div>
-            <div className="flex justify-center place-items-center h-[740px] tall:h-screen bg-[#F9C7C8]">
+            <div className='flex justify-center place-items-center h-[740px] tall:h-screen bg-[#F9C7C8]'>
               {children}
             </div>
           </Fragment>
         </div>
       ) : backgroundType === 'NOLOBBY' ? (
         // 드롭다운이 필요 없는 경우
-        <div className="min-w-[865px] white">
+        <div className='min-w-[865px] white'>
           <Fragment>
-            <div className="flex justify-center place-items-center h-[740px] tall:h-screen bg-[#F9C7C8]">
+            <div className='flex justify-center place-items-center h-[740px] tall:h-screen bg-[#F9C7C8]'>
               {children}
             </div>
           </Fragment>
         </div>
-) : backgroundType === 'ALBUM' ? (
+      ) : backgroundType === 'ALBUM' ? (
         // 사진첩 페이지의 배경
         <div className='min-w-[865px] white'>
           <Fragment>
@@ -46,9 +50,9 @@ return (
         </div>
       ) : (
         // 로그인 페이지의 배경
-        <div className="min-w-[865px] white">
+        <div className='min-w-[865px] white'>
           <Fragment>
-            <div className="flex justify-center pt-16 h-[1150px] 2lg:h-screen bg-[#F9C7C8]">
+            <div className='flex justify-center pt-16 h-[1150px] 2lg:h-screen bg-[#F9C7C8]'>
               {children}
             </div>
           </Fragment>
@@ -57,4 +61,3 @@ return (
     </Fragment>
   );
 }
-
