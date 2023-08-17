6a6c90b606ed5824f03db964822e018cc8996e7a
yebin0825 <yebin0825@naver.com>
2023-08-18 04:30:38 +0900

    [FE] refactor: refactor result correct ui

--- frontend/src/components/common/ResultModal.tsx
+++ frontend/src/components/common/ResultModal.tsx
@@ -114,7 +114,13 @@ export default function ResultModal({ openModal, sendSignal }: IResultModalProps
                       <X size='80' className='z-10' />
                     </div>
                   )}
-                  <div className={'relative top-[-5rem] left-[0.5rem]'}>
+
+                  <div
+                    className={
+                      'relative left-[0.5rem] ' +
+                      `${result.gameResult.correct ? 'top-[-4.4rem]' : 'top-[-5rem]'}`
+                    }
+                  >
                     <img src={result.answerUrl} className='w-20 h-20' />
                   </div>
                 </span>
